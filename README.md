867b7751d912386b697269e406700c7fc54b9596
Ricardo <ri.tapiasaez@gmail.com>
2024-07-25 14:24:40 -0400

    AJAX Call: XMLHttpRequest

---
+++ README.md
@@ -0,0 +1,7 @@
+# Asynchronous JavaScript
+
+##
+
+# Lecture from an Udemy course about JavaScript, guided by <b>Jonas Schmedtmann</b>
+
+![alt text](image.png)
--- script.js
+++ script.js
@@ -7,5 +7,46 @@ const countriesContainer = document.querySelector(".countries");
 
 // THE BASE URL OF THE API
 // https://countries-api-836d.onrender.com/countries/
+// https://restcountries.com/v3.1/name/{name}
 
-console.log("asdf");
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    //   console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const currency = Object.values(data.currencies)[0].name;
+    const language = Object.values(data.languages);
+    // console.log(currency);
+    // console.log(language);
+
+    const html = `
+        <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${language}</p>
+            <p class="country__row"><span>💰</span>${currency}</p>
+          </div>
+        </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+// console.log(request);
+getCountryData("chile");
+getCountryData("portugal");
+// getCountryData("usa");
+// getCountryData("canada");
+// getCountryData("germany");
