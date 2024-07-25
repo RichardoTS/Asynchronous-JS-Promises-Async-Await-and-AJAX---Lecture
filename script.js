3403829b24d67a021a3b445cdd91d91455d751f2
Ricardo <ri.tapiasaez@gmail.com>
2024-07-25 12:58:26 -0400

    Asynchronous JS Promises, Async/Await, and AJAX - Lecture

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////
+
+// THE BASE URL OF THE API
+// https://countries-api-836d.onrender.com/countries/
