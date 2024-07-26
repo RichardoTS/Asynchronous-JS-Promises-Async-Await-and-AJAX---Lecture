1ca6f8a7b86e4b80aa091d808d54d1aa1139336a
Ricardo <ri.tapiasaez@gmail.com>
2024-07-25 20:06:56 -0400

    Consuming Promises

--- script.js
+++ script.js
@@ -73,42 +73,66 @@ const renderCountry = function (data, className = "") {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener("load", function () {
-    //   console.log(this.responseText);
+//   request.addEventListener("load", function () {
+//     //   console.log(this.responseText);
 
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    // Render country 1
-    renderCountry(data);
+//     // Render country 1
+//     renderCountry(data);
 
-    // Get neighbour country 2
-    const [neighbour] = data.borders;
-    console.log(neighbour);
+//     // Get neighbour country 2
+//     const [neighbour] = data.borders;
+//     console.log(neighbour);
 
-    if (!neighbour) return;
+//     if (!neighbour) return;
 
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
-    request2.send();
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener("load", function () {
-      const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
+//     request2.addEventListener("load", function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
 
-      renderCountry(data2, "neighbour");
-    });
-  });
-};
+//       renderCountry(data2, "neighbour");
+//     });
+//   });
+// };
 
 // getCountryAndNeighbour("portugal");
 // getCountryAndNeighbour("peru");
 // getCountryAndNeighbour("chile");
-getCountryAndNeighbour("usa");
+// getCountryAndNeighbour("panama");
+
+// const request = fetch("https://restcountries.com/v3.1/name/portugal");
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+// getCountryData("portugal");
+getCountryData("gb");
